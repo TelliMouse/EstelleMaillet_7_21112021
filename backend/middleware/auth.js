2bd3c21670d4d2f3072a6508e0dc33b03655b951
Estelle Maillet <estellemaillet00@gmail.com>
2021-11-27 17:57:49 +0100

    Modifications middleware auth

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -13,14 +13,15 @@ module.exports = (req, res, next) => {
         const isAdmin = (id) => {
             sql.query(`SELECT * FROM user WHERE id = ${id}`, (error, result) => {
                 if(error) res.statut(400).json({ error });
-                return result[0].admin;
+                //Admin is a BIT type, aka returns 1 for true, 0 for false, and will propably be written as a string ('0' is truthy while 0 is falsy)
+                return parseInt(result[0].admin, 10);
             })
         };
 
-        if(req.body.userId && (req.body.userId === userId || isAdmin(req.body.userId))) {
-            next();
-        } else {
+        if(req.body.user_id && req.body.user_id !== userId && !isAdmin(userId)) {
             res.status(403).json({message: 'Unauthorized request'});
+        } else {
+            next();
         }
 
     } catch (error) {
