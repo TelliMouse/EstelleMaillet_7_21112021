3dfafcd7f7615db6a1092687e82721a43345e4a1
Estelle Maillet <estellemaillet00@gmail.com>
2021-12-01 16:08:26 +0100

    Ajout des router-links dans le component Pages

--- frontend/groupomania/src/components/Pages.vue
+++ frontend/groupomania/src/components/Pages.vue
@@ -1,24 +1,24 @@
 <template>
     <div>
         <div v-if="isThereAPreviousPage">
-            <button>
+            <router-link to="/posts">
                 <img src="../assets/fast-backward-solid.svg" alt="Icone double flèche vers la gauche" />
                 <p>Première page</p>
-            </button>
-            <button>
+            </router-link>
+            <router-link to="{{ previousPageLink }}">
                 <img src="../assets/arrow-circle-left-solid.svg" alt="Icone de flèche vers la gauche"/>
                 <p>Page précèdente</p>
-            </button>
+            </router-link>
         </div>
         <div  v-if="isThereANextPage">
-            <button>
+            <router-link to="{{ nextPageLink }}">
                 <img src="../assets/arrow-circle-right-solid.svg" alt="Icone de flèche vers la droite"/>
                 <p>Page suivante</p>
-            </button>
-            <button>
+            </router-link>
+            <router-link to="{{ lastPageLink }}">
                 <img src="../assets/fast-forward-solid.svg" alt="Icon double flèche vers la droite" />
                 <p>Dernière page</p>
-            </button>
+            </router-link>
         </div>
     </div>
 </template>
@@ -28,39 +28,49 @@ import { mapState } from 'vuex'
 
 export default {
     name: 'Pages',
+    data() {
+        return {
+            currentPage: this.getCurrentPage(),
+            previousPageLink: this.getPreviousPageLink(),
+            nextPageLink: this.getNextPageLink(),
+            lastPageLink: this.getLastPageLink()
+        }
+    },
     computed: {
         ...mapState(['numberOfPages'])
     },
     methods: {
-        isThereAPreviousPage() {
+        getCurrentPage() {
             const url = window.location.href;
             const urlParams = new URLSearchParams(url);
-            let currentPage;
 
             if(!urlParams.get('page')) {
-                currentPage = 1;
+                return 1;
             } else {
-                currentPage = parseInt(urlParams.get('page'), 10);
+                return parseInt(urlParams.get('page'), 10);
             }
-
-            if(currentPage > 1) {
+        },
+        getPreviousPageLink() {
+            const param = this.currentPage - 1;
+            return `/posts?page=${param}`;
+        },
+        getNextPageLink() {
+            const param = this.currentPage + 1;
+            return `/posts?page=${param}`;
+        }   ,
+        getLastPageLink() {
+            const param = this.numberOfPages;
+            return `/posts?page=${param}`;
+        },
+        isThereAPreviousPage() {
+            if(this.currentPage > 1) {
                 return true;
             } else {
                 return false;
             }
         },
         isThereANextPage() {
-            const url = window.location.href;
-            const urlParams = new URLSearchParams(url);
-            let currentPage;
-
-            if(!urlParams.get('page')) {
-                currentPage = 1;
-            } else {
-                currentPage = parseInt(urlParams.get('page'), 10);
-            }
-
-            if(currentPage < this.numberOfPages) {
+            if(this.currentPage < this.numberOfPages) {
                 return true;
             } else {
                 return false;
